7cdf473b3e9960c91541158e9ac295be1cfa3e07
Matthew Peterson <85717822+matthew-peterson-39@users.noreply.github.com>
2024-01-20 22:06:59 -0500

    Initial commit

---
+++ components/auth/new-password-form.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { NewPasswordSchema } from '@/schemas';
+import { useTransition, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { 
+  Form, 
+  FormControl, 
+  FormField, 
+  FormItem, 
+  FormLabel, 
+  FormMessage,
+} from "@/components/ui/form";
+import { CardWrapper } from './card-wrapper';
+import * as z from "zod";
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { FormError } from '../form-error';
+import { FormSuccess } from '../form-success';
+import { newPassword } from '@/actions/new-password';
+
+export const NewPasswordForm = () => {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  
+  const form = useForm<z.infer<typeof NewPasswordSchema>>({
+    resolver: zodResolver(NewPasswordSchema),
+    defaultValues: {
+      password: "",
+    },
+  });
+  
+  const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+    setError("");
+    setSuccess("");
+    
+    startTransition(()=> {
+      newPassword(values, token)
+        .then((data) =>{
+          setError(data?.error);
+          setSuccess(data?.success);
+      })
+    });
+  }
+
+  return (
+    <CardWrapper
+      headerLabel='Enter a new password'
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+      >
+      <Form {...form}>
+        <form 
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='space-y-6'
+        >
+          <div className='space-y-4'>
+            <FormField 
+              control={form.control}
+              name="password"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>New Password</FormLabel>
+                    <FormControl>
+                        <Input
+                          {...field}
+                          disabled={isPending}
+                          placeholder='******'
+                          type='password'
+                        />
+                    </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+              />
+          </div>
+          <FormError message={error}></FormError>
+          <FormSuccess message={success}></FormSuccess>
+          <Button
+            type='submit'
+            className="w-full"
+            disabled={isPending}
+          >
+            Reset password
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  )};
---
+++ lib/tokens.ts
@@ -0,0 +1,75 @@
+import { getVerificationTokenByEmail } from "@/data/verificationToken";
+import { db } from "./db";
+import {v4 as uuidv4} from "uuid";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+import crypto from "crypto"
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
+
+export const generateTwoFactorToken = async (email: string) => {
+    const token = crypto.randomInt(100_000, 1_000_000).toString();
+    const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
+
+    const existingToken = await getTwoFactorTokenByEmail(email);
+    if (existingToken) {
+        await db.twoFactorToken.delete({
+            where: {id: existingToken.id}
+        });
+    }
+
+    const twoFactorToken = await db.twoFactorToken.create({
+        data: {
+            email,
+            token, 
+            expires
+        }
+    });
+
+    return twoFactorToken;
+}
+
+export const generatePasswordResetToken = async (email: string) => {
+    const token = uuidv4();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);   // Expire in 1 hr
+    const exisitingToken = await getPasswordResetTokenByEmail(email);
+
+    if(exisitingToken) {
+        await db.passwordResetToken.delete({
+            where: {id: exisitingToken.id}
+        });
+    }
+
+    const passwordResetToken = await db.passwordResetToken.create({
+        data: {
+            email,
+            token,
+            expires
+        }
+    });
+
+    return passwordResetToken
+}
+
+export const generateVerificationToken = async (email: string) => {
+    const token = uuidv4();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);   // Expire in 1 hr
+
+    const exisitingToken = await getVerificationTokenByEmail(email);
+    
+    if (exisitingToken) {
+        await db.verificationToken.delete({
+            where: {
+                id: exisitingToken.id,
+            },
+        });
+    }
+
+    const verificationToken = await db.verificationToken.create({
+        data: {
+            email, 
+            token, 
+            expires,
+        }
+    });
+    
+    return verificationToken
+}

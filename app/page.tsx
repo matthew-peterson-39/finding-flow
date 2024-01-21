a2adf35f8693afe138b10be45b059048b81178cf
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-21 10:13:22 -0500

    style for responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,17 @@ const FONT = Poppins({subsets: ["latin"], weight:["600"]})
 export default function Home() {
   return (
   <>
-    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-br from-zinc-700 to-slate-800">
-      <div className="flex flex-col items-center justify-center space-y-6 text-center">
-        <h1 className={cn("text-6xl text-center font-semibold text-white drop-shadow-md", FONT.className)}>
+    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-br from-zinc-700 to-slate-800 px-4">
+      <div className="flex flex-col items-center justify-center space-y-4 md:space-y-6 text-center">
+        <h1 className={cn("text-4xl md:text-6xl text-center font-semibold text-white drop-shadow-md", FONT.className)}>
           Finding Flow
         </h1>
-        <p className="text-white text-lg">
+        <p className="text-white text-base md:text-lg">
           Find your flow, own your life.
         </p>
         <div className="flex">       
           <LoginButton mode="modal" asChild>
-            <Button variant={"secondary"} size={"lg"}>
+            <Button variant={"secondary"} size={"default"}>
               Sign in
             </Button>
           </LoginButton>

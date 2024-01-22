db25ddba747900ddf46db852ecaf7598ec7239a2
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-21 21:22:36 -0500

    adjust email

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,7 +5,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
     await resend.emails.send({
-        from: "onboarding@find-flow.com",
+        from: "auth@find-flow.com",
         to: email,
         subject: "2FA code",
         html: `Your 2FA code: ${token}`
@@ -15,7 +15,7 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
     const resetLink = `${domain}/auth/new-password?token=${token}`
     await resend.emails.send({
-        from: "onboarding@find-flow.com",
+        from: "auth@find-flow.com",
         to: email,
         subject: "Reset password",
         html: `Click the link: \n\n${resetLink}\n\n to reset password.`

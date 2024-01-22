790fe24efa8305a8c437e6f52faff74e16909c07
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-22 10:28:49 -0500

    update email from name

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,7 +5,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
     await resend.emails.send({
-        from: "Flow <noreply@find-flow.com>",
+        from: "Find Flow <noreply@find-flow.com>",
         to: email,
         subject: "2FA code",
         html: `Your 2FA code: ${token}`
@@ -15,7 +15,7 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
     const resetLink = `${domain}/auth/new-password?token=${token}`
     await resend.emails.send({
-        from: "Flow <noreply@find-flow.com>",
+        from: "Find Flow <noreply@find-flow.com>",
         to: email,
         subject: "Reset password",
         html: `Click the link: \n\n${resetLink}\n\n to reset password.`
@@ -26,7 +26,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
-        from: "Flow <noreply@find-flow.com>",
+        from: "Find Flow <noreply@find-flow.com>",
         to: email,
         subject: "Confirm your email",
         html: `Click the link: ${confirmLink} to verify email.`
